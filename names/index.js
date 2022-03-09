5cae0b0f1de417f85c0cda6487bd0f252398ad16
sarathbwd <sarathbwdr@gmail.com>
2022-03-09 05:58:08 +0000

    First name of the people-solution

---
+++ names/index.js
@@ -0,0 +1,9 @@
+const peopleList = require("../country/state/city/index");
+
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peopleList) => {
+  return getFirstNames(peopleList);
+};
+
+module.exports = getPeopleInCity;
